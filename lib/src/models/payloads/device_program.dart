875673a288d7ca414ed2c1dd8bbd9d60ab11d491
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-02-14 23:29:06 -0600

    chore: remove useless setter

--- lib/src/models/payloads/device_program.dart
+++ lib/src/models/payloads/device_program.dart
@@ -1,15 +1,10 @@
-
 import 'package:flutter_home_connect_sdk/flutter_home_connect_sdk.dart';
 
 class DeviceProgram {
   final String key;
-  final List<DeviceOptions> options;
+  List<DeviceOptions> options;
 
   DeviceProgram(this.key, this.options);
-
-  set options(List<DeviceOptions> options) {
-    this.options = options;
-  }
 
   factory DeviceProgram.fromPayload(Map<String, dynamic> payload) {
     return DeviceProgram(payload['key'], []);
