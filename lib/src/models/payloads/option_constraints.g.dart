5e4d9cfceac727878c4bd4c09d3925929152ff04
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-02-20 16:33:04 -0600

    refactor: change name from DeviceConstrains to OptionsConstrains

--- lib/src/models/payloads/option_constraints.g.dart
+++ lib/src/models/payloads/option_constraints.g.dart
@@ -1,19 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'device_constrains.dart';
+part of 'option_constraints.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-DeviceConstrains _$DeviceConstrainsFromJson(Map<String, dynamic> json) =>
-    DeviceConstrains(
+OptionConstraints _$OptionConstraintsFromJson(Map<String, dynamic> json) =>
+    OptionConstraints(
       min: json['min'] as int? ?? 0,
       max: json['max'] as int? ?? 100,
-      stepsize: json['stepsize'] as int? ?? 5,
+      stepsize: json['stepsize'] as int? ?? 1,
     );
 
-Map<String, dynamic> _$DeviceConstrainsToJson(DeviceConstrains instance) =>
+Map<String, dynamic> _$OptionConstraintsToJson(OptionConstraints instance) =>
     <String, dynamic>{
       'min': instance.min,
       'max': instance.max,
