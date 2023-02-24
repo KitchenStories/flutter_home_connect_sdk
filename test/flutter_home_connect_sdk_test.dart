e0b61daf56f9fcd726f9f5747acf2bffdd620a3b
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-02-24 12:12:50 -0600

    chore: move back tests under test folder

---
+++ test/flutter_home_connect_sdk_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_home_connect_sdk/src/flutter_home_connect_sdk_base.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
