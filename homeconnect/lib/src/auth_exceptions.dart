7feb11f0a781fb007405c681cab159f4f49d45d6
Essau Ramirez <sauramirez@users.noreply.github.com>
2023-02-28 21:44:34 -0600

    Adds auth exceptions

---
+++ homeconnect/lib/src/auth_exceptions.dart
@@ -0,0 +1,20 @@
+class AuthException implements Exception {
+  final String message;
+
+  AuthException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class RefreshTokenException extends AuthException {
+  RefreshTokenException(super.message);
+}
+
+class OauthCodeException extends AuthException {
+  OauthCodeException(super.message);
+}
+
+class InvalidTokenException extends AuthException {
+  InvalidTokenException(super.message);
+}
