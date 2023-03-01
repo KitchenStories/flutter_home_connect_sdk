263a8e03d9c2f1f6d5f1a0150a8854ec41a06862
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-03-01 14:53:06 -0600

    chore: add export files

--- homeconnect/lib/homeconnect.dart
+++ homeconnect/lib/homeconnect.dart
@@ -10,6 +10,8 @@ export 'src/models/programs/device_program.dart';
 export 'src/models/status/device_status.dart';
 export 'src/models/info/device_info.dart';
 export 'src/models/devices/oven_device.dart';
+export 'src/home_device.dart';
+export 'client/client_dart.dart';
 
 
 // TODO: Export any libraries intended for clients of this package.
