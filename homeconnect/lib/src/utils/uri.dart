a30be14998de92c51016beabb2bf19422958fcb0
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-03-23 18:16:56 -0600

    fix: remove trailing slash

--- homeconnect/lib/src/utils/uri.dart
+++ homeconnect/lib/src/utils/uri.dart
@@ -2,6 +2,6 @@ import 'dart:core';
 
 extension JoinUri on Uri {
   Uri join(String path) {
-    return resolve(path);
+    return resolve(path.endsWith('/') ? path.substring(0, path.length - 1) : path);
   }
 }
