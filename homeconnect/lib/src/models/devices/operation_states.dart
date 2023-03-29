f5fcd6e24577b5034ca62fec10c626c030f25e1a
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2023-03-28 18:13:16 -0600

    fix: add inactive operation state

--- homeconnect/lib/src/models/devices/operation_states.dart
+++ homeconnect/lib/src/models/devices/operation_states.dart
@@ -1,8 +1,9 @@
-enum OperationStatesEnum { run, ready }
+enum OperationStatesEnum { run, ready, inactive }
 
 final Map<OperationStatesEnum, String> _operationStatesMap = {
   OperationStatesEnum.run: 'BSH.Common.EnumType.OperationState.Run',
   OperationStatesEnum.ready: 'BSH.Common.EnumType.OperationState.Ready',
+  OperationStatesEnum.inactive: 'BSH.Common.EnumType.OperationState.Inactive',
 };
 
 String operationState({required OperationStatesEnum state}) {
